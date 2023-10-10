6d68524e800d353641cd016d685e1cddd8ea563f
Jack Kelly <jack@openclimatefix.org>
2023-10-10 21:35:16 +0100

    Define multiple Datasets

--- recipes/slow_memcpy.py
+++ recipes/slow_memcpy.py
@@ -5,6 +5,7 @@ on Thu 28th Sept 2023.
 See https://github.com/zarr-developers/zarr-benchmark/issues/5
 """
 
+from pathlib import Path
 from typing import Final
 
 import numpy as np
@@ -20,23 +21,36 @@ _COLS_PER_CHUNK: Final[int] = 1_000
 # Each batch reads all columns, but just one chunk of rows.
 _NUM_BATCHES: Final[int] = _ROWS_TOTAL // _ROWS_PER_CHUNK
 
-class SlowMemcpyDataset(Dataset):
+class SlowMemcpyDatasetAllZeroLZ4(Dataset):
     def create(self) -> None:
-        array = np.zeros((_ROWS_TOTAL, _COLS_TOTAL), dtype=np.int16)
+        array = _all_zero_array()
         # Note that, by default, zarr.save uses LZ4 compression.
-        zarr.save(self.path, array, mode='w', chunks=(_ROWS_PER_CHUNK, _COLS_PER_CHUNK))
+        zarr.save(
+            self.path,
+            array, 
+            mode='w',
+            chunks=(_ROWS_PER_CHUNK, _COLS_PER_CHUNK))
+        
+class SlowMemcpyDatasetAllZeroUncompressed(Dataset):
+    def create(self) -> None:
+        array = _all_zero_array()
+        array = zarr.array(array, chunks=(_ROWS_PER_CHUNK, _COLS_PER_CHUNK), compressor=None)
+        zarr.save(self.path, array, mode='w')
+        
 
+def _all_zero_array() -> np.ndarray:
+    return np.zeros((_ROWS_TOTAL, _COLS_TOTAL), dtype=np.int16)
 
 class SlowMemcpyWorkload(Workload):
-    def init_dataset(self) -> SlowMemcpyDataset:
-        return SlowMemcpyDataset()
+    def init_datasets(self) -> tuple[Dataset]:
+        return (SlowMemcpyDatasetAllZeroLZ4(), SlowMemcpyDatasetAllZeroUncompressed())
     
-    def run(self) -> None:
+    def run(self, dataset_path: Path) -> None:
         """Load Zarr into RAM, in batches.
         
         Each batch reads all columns, but just one chunk of rows.
         """
-        array = zarr.open(self.dataset.path, mode='r').arr_0
+        array = zarr.open(dataset_path, mode='r').arr_0
         for i in range(_NUM_BATCHES):
             row_slice = slice(i * _ROWS_PER_CHUNK, (i+1) * _ROWS_PER_CHUNK)
             array.get_orthogonal_selection((row_slice, slice(0, _COLS_TOTAL)))
